ca9a6bed1f0f7bb074955802b7283e1de0911dd0
Henrik Uggla <henrik.uggla@kristianstad.se>
2019-09-05 14:52:36 +0200

    Update onbuildrun.sh

--- build/rootfs/usr/sbin/onbuildrun.sh
+++ build/rootfs/usr/sbin/onbuildrun.sh
@@ -218,7 +218,7 @@ then
    echo '++++++++ FINALCMDS <begin> +++++++'
    echo '++++++++++++++++++++++++++++++++++'
    set -x
-   chroot /finalfs sh -c "exec > /build.log 2>&1 && set -ex +fam && eval \"\$FINALCMDS\""
+   chroot /finalfs /bin/sh -c "set -ex +fam && eval \"\$FINALCMDS\""
    set +x
    echo '----------------------------------'
    echo '-------- FINALCMDS </end> --------'
